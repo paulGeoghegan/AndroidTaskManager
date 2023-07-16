65aaff04bf41aa1bd0bc299c9f20a64360361a23
paulGeoghegan <56843333+paulGeoghegan@users.noreply.github.com>
2023-07-17 00:03:49 +0100

    Added more form fields and did some basic styling

--- Screens/AddTasksScreen.tsx
+++ Screens/AddTasksScreen.tsx
@@ -13,16 +13,18 @@ import {
 } from 'react-native';
 import ModalDropdown from 'react-native-modal-dropdown';
 
+let categoryOptions = ['All'];
+
 const AddTasksScreen = ({ navigation }:{navigation : any}) =>{
 	return (
 		<Formik
-			initialValues={{title:'',description:''}}
+			initialValues={{title:'',description:'',category:'',difficulty:'',startDate:'',repeats:'',intervalNumber:'',intervalType:''}}
 			onSubmit={values => console.log(values)}
 		>
 		{({handleChange, handleBlur, handleSubmit, values }) => (
 				<View style={styles.container}>
 					<View accessible={true}>
-						<Text accessibilityElementsHidden={true} importantForAccessibility='no'>Title</Text>
+						<Text accessibilityElementsHidden={true} importantForAccessibility='no' style={styles.basicText}>Title</Text>
 						<TextInput
 							accessibilityLabel='Title'
 							autoCapitalize='words'
@@ -30,11 +32,12 @@ const AddTasksScreen = ({ navigation }:{navigation : any}) =>{
 							onChangeText={handleChange('title')}
 							onBlur={handleBlur('title')}
 							maxLength={30}
+							style={styles.basicText}
 							value={values.title}
 						/>
 					</View>
 					<View accessible={true}>
-						<Text accessibilityElementsHidden={true} importantForAccessibility='no'>Description</Text>
+						<Text accessibilityElementsHidden={true} importantForAccessibility='no' style={styles.basicText}>Description</Text>
 						<TextInput
 							accessibilityLabel='Description'
 							autoCapitalize='sentences'
@@ -42,16 +45,45 @@ const AddTasksScreen = ({ navigation }:{navigation : any}) =>{
 							onBlur={handleBlur('title')}
 							maxLength={300}
 							multiline
+							style={styles.basicText}
 							value={values.description}
 						/>
 					</View>
 					<View accessible={true}>
-						<Text accessibilityElementsHidden={true} importantForAccessibility='no'>Category</Text>
-						<ModalDropdown defaultValue='Select a category...' showSearch options={['All']}/>
+						<Text accessibilityElementsHidden={true} importantForAccessibility='no' style={styles.basicText}>Category</Text>
+						<ModalDropdown
+							defaultTextStyle={styles.basicText}
+							defaultValue='Select a category...'
+							onSelect={handleChange('category')}
+							options={categoryOptions}
+							showSearch
+						/>
 					</View>
 					<View accessible={true}>
 
 					</View>
+					<View accessible={true}>
+						<Text accessibilityElementsHidden={true} importantForAccessibility='no' style={styles.basicText}>Repeats every</Text>
+						<View style={styles.inlineContainer}>
+							<TextInput
+								accessibilityLabel='Interval number'
+								inputMode='numeric'
+								onChangeText={handleChange('intervalNumber')}
+								onBlur={handleBlur('intervalNumber')}
+								maxLength={5}
+								style={styles.basicText}
+								value={values.intervalNumber}
+							/>
+							<ModalDropdown
+								defaultTextStyle={styles.basicText}
+								defaultValue='Select an interval type...'
+
+								onSelect={handleChange('intervalType')}
+								options={['Days','Weeks','Months','Years']}
+							/>
+						</View>
+					</View>
+
 					<Button onPress={handleSubmit} title='Add Task'/>
 				</View>
 		)}
@@ -60,11 +92,15 @@ const AddTasksScreen = ({ navigation }:{navigation : any}) =>{
 };
 
 const styles = StyleSheet.create({
+	basicText:{
+		color:'black'
+	},
 	container:{
 		flex:1
 	},
-	Text:{
-		color:'black'
+	inlineContainer:{
+		alignItems:'center',
+		flexDirection:'row'
 	},
 });
 
